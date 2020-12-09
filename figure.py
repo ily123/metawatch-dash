ily123/metawatch-dash

commit 9dfba13b4e5a32e0d67c7443e56872a49f0745bf

Refactored xaxis creation in Ridgeplot to be more DRY

figure.py
@@ -406,20 +406,14 @@ class RidgePlot:
         fig.update_layout(annotations=self._keep_annotations("all"))
         max_x = self.summary.best_key.max()
         min_x = -2.5 if max_x < 20 else -6 # this is a lazy way to fix label position
-        xaxis = dict(
-            title="<b>KEY LEVEL</b>",
-            range=[min_x, max_x - 1],
-            tickvals=[0] + list(range(3, max_x - 1, 5)),
-            ticktext=["+2"] + ["+" + str(i + 2) for i in range(3, max_x - 1, 5)],
-        )
 
-        xaxis2 = dict(
+        xaxis_template = dict(
             range=[min_x, max_x - 1],
             tickvals=[0] + list(range(3, max_x - 1, 5)),
             ticktext=["+2"] + ["+" + str(i + 2) for i in range(3, max_x - 1, 5)],
-            side="top",
-            overlaying="x",
         )
+        xaxis = dict(title="<b>KEY LEVEL</b>").update(xaxis_template)
+        xaxis2 = dict(side="top",overlaying="x").update(xaxis_template)
 
         bin_ymax = self.data.to_numpy().max()  # tallest spec/key bin
         ymax = 36 * self._calculate_vertical_offset() + bin_ymax + (bin_ymax * 0.1)

commit db1fe7e2f2bf2437e34442158307ad7c5f164211

Fixed invisible xaxis
Commit 9dfba13 broke the xaxis var. Dict updates are in-place
and return None. Fixed to update axis dicts properly.

figure.py
@@ -412,8 +412,10 @@ class RidgePlot:
             tickvals=[0] + list(range(3, max_x - 1, 5)),
             ticktext=["+2"] + ["+" + str(i + 2) for i in range(3, max_x - 1, 5)],
         )
-        xaxis = dict(title="<b>KEY LEVEL</b>").update(xaxis_template)
-        xaxis2 = dict(side="top",overlaying="x").update(xaxis_template)
+        xaxis = dict(title="<b>KEY LEVEL</b>")
+        xaxis.update(xaxis_template)
+        xaxis2 = dict(side="top",overlaying="x")
+        xaxis2.update(xaxis_template)
 
         bin_ymax = self.data.to_numpy().max()  # tallest spec/key bin
         ymax = 36 * self._calculate_vertical_offset() + bin_ymax + (bin_ymax * 0.1)