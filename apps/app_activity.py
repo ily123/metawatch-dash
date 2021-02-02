78d965642fe6e19e5dbaa3c0b0827d85d15d3f58
ily123 <ibnovikov@gmail.com>
2021-02-02 17:30:59 -0600

    a bit of clean up on activity page

--- apps/app_activity.py
+++ apps/app_activity.py
@@ -1,15 +1,9 @@
-import math
-import time
-
-import blizzcolors
 import constructor
 import dash_core_components as dcc
 import dash_html_components as html
 import dataserver
 import figure
-import pandas as pd
 from app import app
-from dash.dependencies import Input, Output, State
 
 DB_FILE_PATH = "data/summary.sqlite"
 dataserver_ = dataserver.DataServer(DB_FILE_PATH)
@@ -18,6 +12,20 @@ fig_ = figure.BasicBarChart(data)
 fig = fig_.draw_figure()
 fig = constructor.annotate_weekly_figure(fig)
 
+fig_config = dict(
+    modeBarButtonsToRemove=[
+        "zoomIn2d",
+        "zoomOut2d",
+        "hoverClosestCartesian",
+        "hoverCompareCartesian",
+        "toggleSpikelines",
+        "autoScale2d",
+        "lasso2d",
+        "select2d",
+    ],
+    displaylogo=False,
+)
+
 layout = html.Div(
     [
         html.H3("PLAYER ACTIVITY"),
@@ -25,6 +33,6 @@ layout = html.Div(
             """This figure shows the number of runs recorded each week
             since the start of BFA S4."""
         ),
-        dcc.Graph(figure=fig),
+        dcc.Graph(figure=fig, config=fig_config),
     ]
 )
