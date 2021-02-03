a24cad3d3535c5f2b1f501c66e69c2d5263430fe
ily123 <ibnovikov@gmail.com>
2021-02-03 09:06:02 +0000

    added tracking script to app body

--- app.py
+++ app.py
@@ -2,3 +2,24 @@ import dash
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
 server = app.server
+app.index_string = """<!DOCTYPE html>
+    <html>
+    <head>
+      <!-- Plausible.io tracking script -->
+      <script async defer data-domain="benched.me" src="https://plausible.io/js/plausible.js"></script>
+      <!-- End Plausible.io tracking script -->
+    {%metas%}
+    <title>{%title%}</title>
+    {%favicon%}
+    {%css%}
+    </head>
+    <body>
+    {%app_entry%}
+    <footer>
+    {%config%}
+    {%scripts%}
+    {%renderer%}
+    </footer>
+    </body>
+    </html>
+    """
