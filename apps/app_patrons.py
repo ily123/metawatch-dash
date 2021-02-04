b60ec559f14a540fad02fdb49fc64b80a969e087
ily123 <ibnovikov@gmail.com>
2021-02-04 07:30:30 +0000

    grammar

--- apps/app_patrons.py
+++ apps/app_patrons.py
@@ -8,7 +8,7 @@ layout = html.Div(
     [
         html.H3("CREDITS"),
         html.P("This is where Patron names will go. Once we have some patrons. We will totally have patrons."),
-        html.P("If you are interested becoming the FIRST PATRON, see our Patreon at https://www.patreon.com/benched", id="patreon-alert"),
+        html.P("If you are interested in becoming the FIRST PATRON, see our Patreon at https://www.patreon.com/benched", id="patreon-alert"),
         html.P("If you sign up and something doesn't work, don't panic, we will figure it out."),
         html.Br(),
         html.Br(),
